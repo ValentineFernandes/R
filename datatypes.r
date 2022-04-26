6e5d145dae5792f09196c93eb0db97a8230834ae
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 03:19:40 -0800

    Create datatypes.r

---
+++ datatypes.r
@@ -0,0 +1,19 @@
+​# numeric
+x <- 10.5
+class(x)
+
+# integer
+x <- 1000L
+class(x)
+
+# complex
+x <- 9i + 3
+class(x)
+
+# character/string
+x <- "R is exciting"
+class(x)
+
+# logical
+x <- TRUE
+class(x)
